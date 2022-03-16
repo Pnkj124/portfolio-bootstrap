f09c4d3fa6994c95e1112efccf7a12477ac91e52
Pankaj Chaudhary <67068794+Pnkj124@users.noreply.github.com>
2022-03-16 03:36:40 +0100

    add initial files

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        projects: [],
+    },
+    mounted() {
+        fetch('https://gh-pinned-repos.egoist.sh/?username=Pnkj124')
+            .then(res => res.json())
+            .then(res => {
+                for (const repo of res) {
+                    this.projects.push({
+                        project_icon: "./img/github.svg",
+                        project_title: repo.repo,
+                        project_description: repo.description,
+                        url: repo.link,
+                        language: repo.language,
+                        stars: repo.stars,
+                        forks: repo.forks
+                    })
+                }
+            });
+    }
+})
